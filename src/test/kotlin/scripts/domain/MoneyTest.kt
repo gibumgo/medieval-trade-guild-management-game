27af21f8a3ae8fad8de49ae3575ed712d06cfcec
gibumgo <gibumgo@gmail.com>
2025-11-12 14:28:00 +0900

    test: 골드 게임 재화 생성 및 유효성 테스트

---
+++ src/test/kotlin/scripts/domain/MoneyTest.kt
@@ -0,0 +1,33 @@
+package scripts.domain
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class MoneyTest {
+    @Test
+    @DisplayName("생성 테스트")
+    fun createMoneyTest() {
+        val money = Money.of(100)
+        assertEquals(100, money.amount)
+    }
+
+    @Test
+    @DisplayName("0 이하 금액 입력시 예외 발생")
+    fun invalidTest() {
+        val exception = assertThrows<IllegalArgumentException>  {
+            Money.of(0)
+        }
+        assertEquals(ErrorMessage.MONEY_ERROR, exception.message)
+    }
+
+    @Test
+    @DisplayName("음수 금액 입력시 예외 발생")
+    fun createMoneyNegativeTest() {
+        val exception = assertThrows<IllegalArgumentException>  {
+            Money.of(-50)
+        }
+        assertEquals(ErrorMessage.MONEY_ERROR, exception.message)
+    }
+}
