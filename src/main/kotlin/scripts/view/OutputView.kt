615a4e3e6b188e1091c901d388e8ceb2910c3269
gibumgo <gibumgo@gmail.com>
2025-11-15 19:22:19 +0900

    feat: 플레이어 상태 출력 기능 추가

--- src/main/kotlin/scripts/view/OutputView.kt
+++ src/main/kotlin/scripts/view/OutputView.kt
@@ -11,4 +11,12 @@ class OutputView {
         println("")
     }
 
+    fun printPlayerStatus(playerDTO: PlayerDTO) {
+        println("[상단 현황]")
+        println("골드(G): ${playerDTO.gold}")
+        println("명성: ${playerDTO.reputation}")
+        println("창고: ${playerDTO.capacityUsed} / ${playerDTO.capacityMax}")
+        println("보유 행상대: ${playerDTO.caravans.size}대")
+    }
+
 }
