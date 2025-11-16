9e90ef43cd461af099e51d29dadb758e74e827fc
gibumgo <gibumgo@gmail.com>
2025-11-16 20:41:56 +0900

    feat: 계속 진행 및 종료 입력 기능 추가

--- src/main/kotlin/scripts/view/InputView.kt
+++ src/main/kotlin/scripts/view/InputView.kt
@@ -8,9 +8,21 @@ class InputView {
         return Console.readLine().trim().uppercase() == "Y"
     }
 
-    fun inputSelectNumber () : Int{
+    fun inputSelectNumber(): Int {
         return Console.readLine().trim().toInt()
     }
 
-
+    fun waitForEnterOnly(): Boolean {
+        while (true) {
+            val input = Console.readLine().trim()
+            if (input.isEmpty()) {
+                return true
+            }
+            if (input.toInt() == 0) {
+                println("프로그램을 종료합니다.")
+                Console.close()
+            }
+            println("엔터만 눌러주세요. (0 = 종료)")
+        }
+    }
 }
