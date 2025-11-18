b0c5c39b7f98550540f8d42155d4ff9c7576bb6f
gibumgo <gibumgo@gmail.com>
2025-11-18 13:44:46 +0900

    refactor: 더미 아이템 추가

--- src/main/kotlin/scripts/domain/testObject/TestItems.kt
+++ src/main/kotlin/scripts/domain/testObject/TestItems.kt
@@ -7,4 +7,9 @@ object TestItems {
     val wheat = listOf(
         ItemSlot.of(Item.of("밀", 1), 10)
     )
+
+    val wood = ItemSlot.of(Item.of("목재", 1), 5)
+    val spice = ItemSlot.of(Item.of("향료", 1), 2)
+    val iron = ItemSlot.of(Item.of("철", 1), 20)
+
 }
