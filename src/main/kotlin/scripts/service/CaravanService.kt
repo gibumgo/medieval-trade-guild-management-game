e212de30c56360abc2a3887496f5090d67b11f33
gibumgo <gibumgo@gmail.com>
2025-11-24 06:23:05 +0900

    refactor: CaravanService에 캐러밴 선택,출발 기능 추가

--- src/main/kotlin/scripts/service/CaravanService.kt
+++ src/main/kotlin/scripts/service/CaravanService.kt
@@ -19,6 +19,23 @@ class CaravanService(
 
     fun removePlayerCaravan(caravan: Caravan) = caravanRepository.removeFromPlayer(caravan)
 
+    fun selectAndStartTrip(inputIndex: Int): Caravan {
+        val caravan = selectAvailableCaravan(inputIndex)
+        return startTrip(caravan)
+    }
+
+    private fun selectAvailableCaravan(inputIndex: Int): Caravan {
+        val caravans = availableCaravans()
+        require(inputIndex in 1..caravans.size) { "번호 선택이 범위를 벗어났습니다." }
+        return caravans[inputIndex - 1]
+    }
+
+    private fun startTrip(caravan: Caravan): Caravan {
+        val travelingCaravan = caravan.startTrip()
+        updateCaravan(travelingCaravan)
+        return travelingCaravan
+    }
+
     companion object {
         private const val DEFAULT_SPEED = 1
     }
