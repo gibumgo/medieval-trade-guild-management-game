370d0c1682623a2ba3e697db101b3ab4b0e9d067
gibumgo <gibumgo@gmail.com>
2025-12-01 00:33:57 +0900

    refactor: 활성화된 퀘스트 불러오는 기능 수정

--- src/main/kotlin/scripts/service/QuestService.kt
+++ src/main/kotlin/scripts/service/QuestService.kt
@@ -5,19 +5,22 @@ import scripts.domain.caravan.Caravan
 import scripts.domain.player.Player
 import scripts.domain.quest.Quest
 import scripts.domain.quest.QuestDelivery
-import scripts.repository.QuestsRepository
+import scripts.domain.quest.QuestStatus
+import scripts.repository.QuestDeliveryRepository
+import scripts.repository.QuestRepository
 
 class QuestService(
-    private val questRepository: QuestsRepository
+    private val questRepository: QuestRepository,
+    private val questDeliveryRepository: QuestDeliveryRepository,
 ) {
-
     fun fillerActive(inventory: List<ItemSlot>) {
-        questRepository.findAll().forEach { quest: Quest ->
-            quest.activateWith(inventory)
+        questRepository.findByState(QuestStatus.INACTIVE).forEach { quest: Quest ->
+            val activeQuest = quest.activateWith(inventory)
+            questRepository.update(activeQuest)
         }
     }
 
-    fun availableQuest(): List<Quest> = questRepository.findActive()
+    fun availableQuest(): List<Quest> = questRepository.findByState(QuestStatus.ACTIVE)
 
     fun selectedQuest(inputNumber: Int): Quest {
         require(inputNumber in 1..availableQuest().size) { "번호 선택이 범위를 벗어났습니다." }
