cffb25324e2142311ef983ecdc591674b6a1821b
gibumgo <gibumgo@gmail.com>
2025-11-30 23:14:02 +0900

    feat: 행상대 상태 확인 기능 추가

--- src/main/kotlin/scripts/domain/caravan/CaravanStatus.kt
+++ src/main/kotlin/scripts/domain/caravan/CaravanStatus.kt
@@ -3,5 +3,11 @@ package scripts.domain.caravan
 enum class CaravanStatus(val displayName: String) {
     READY("대기중"),
     TRAVELING("이동중"),
-    COMPLETED("완료")
+    COMPLETED("완료");
+
+    fun isReady(): Boolean = this == READY
+
+    fun isTravel(): Boolean = this == TRAVELING
+
+    fun isCompleted(): Boolean  = this == COMPLETED
 }
